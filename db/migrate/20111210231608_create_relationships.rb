83743f2204e2c9c666e56bb34bda4ba08ecaa770
Stanley H. Lui <stnhhl2000@yahoo.com>
2011-12-12 10:37:20 -0800

    Added user following

---
+++ db/migrate/20111210231608_create_relationships.rb
@@ -0,0 +1,30 @@
+class CreateRelationships < ActiveRecord::Migration
+
+  def self.up
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], :unique => true
+  end
+
+  def self.down
+    drop_table :relationships
+  end
+
+
+
+#  def change
+#    create_table :relationships do |t|
+#      t.integer :follower_id
+#      t.integer :followed_id
+
+#      t.timestamps
+#    end
+#  end
+
+end
