38ea7707eb392592103d78aba9debc10d72fdba1
Stanley H. Lui <stnhhl2000@yahoo.com>
2011-12-12 14:27:59 -0800

    At the end of chapter 12 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Sample1::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false   <---- original  12/12/11 Stan Lui
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
